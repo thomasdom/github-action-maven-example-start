58c7fea3171bc74c1e2679a5d5820186d0f8fed9
Thomas Domingues <thomasdomingues@icloud.com>
2021-01-28 15:43:31 +0100

    Add junit and a test

---
+++ src/test/java/TestDeepThought.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TestDeepThought {
+    @Test
+    @DisplayName("Test the answer to everything")
+    void testAnswerToAll() {
+        DeepThought superComputer = new DeepThought();
+        assertEquals(42, superComputer.answer_to_the_ultimate_question_of_life_the_universe_and_everything());
+    }
+}
